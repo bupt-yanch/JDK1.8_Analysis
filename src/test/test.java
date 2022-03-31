c8fca638db340bcd36d281b6c01786d6e09e711d
yanch <bupt-yanch@users.noreply.github.com>
2022-03-31 19:52:19 +0800

    init

---
+++ src/test/test.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class test {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1,1);
+        System.out.println(map);
+    }
+}
